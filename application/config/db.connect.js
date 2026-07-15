f7e54e1915a1b5380d88c358ee2eb1ee603efdfd
AshwiniNaik01 <ashwiniuttirnaservice02@gmail.com>
2026-07-15 12:12:39 +0530

    update

--- application/config/db.connect.js
+++ application/config/db.connect.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool(
     database: process.env.DB_DATABASE,
     // Password
     password: process.env.DB_PASSWORD,
-    // Wait for connection 
+    // Wait for connections
     waitForConnections: true,
     // Port
     port: process.env.DB_PORT,
