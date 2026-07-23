867762526aad68b378997744064509e1983a7044
AshwiniNaik01 <ashwiniuttirnaservice02@gmail.com>
2026-07-23 17:37:12 +0530

    cors added

--- app.js
+++ app.js
@@ -28,16 +28,34 @@ app.use(session({
   // }
 }));
 
-app.use(cors())
+const allowedOrigins = [
+  'https://api.sklandmark.com',
+  'https://www.api.sklandmark.com',
+  'https://sklandmark.com',
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:3000',
+  'https://www.sklandmark.com',
+];
 
-// app.use(cors(
-//   {
-//   //   origin: process.env.FRONTEND_URL,
-//     // credentials: true,
-//   //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   //   allowedHeaders: ['Content-Type', 'Authorization']
-//   }
-// ));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      // In case we want to support any origin in dev/testing:
+      if (process.env.NODE_ENV !== 'PROD') {
+        return callback(null, true);
+      }
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(upload());
 
